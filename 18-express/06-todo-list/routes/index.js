b1fab90b68a275da8c0e32e0f3e548b57cac5a13
pankaiya <3131225624@qq.com>
2018-11-28 15:05:04 +0800

    利用express框架实现todo-list功能

--- 18-express/06-todo-list/routes/index.js
+++ 18-express/06-todo-list/routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var items=[];
+var items = [];
 
-/* GET home page. */
 router.get('/', function(req, res/*, next*/) {
-  console.log(req.url);
-  res.render('index', { items: items });
+    console.log(req.url);
+      res.render('index', {items: items});
+
 });
 
-router.post('/',function(req,res){
-  if(req.body.item!=='')items.push(req.body.item);
-  res.render('index',{items:items});
+router.post('/', function(req, res){
+    if(req.body.item !== '') items.push(req.body.item);
+      res.render('index', {items: items});
+
 });
 
 module.exports = router;
